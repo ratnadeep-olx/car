de27d53b4680b734986026d47f032e0c13c5d27f
Your Name <you@example.com>
2018-01-31 16:04:27 +0000

    Latest

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
 # all the imports
 import os
+import json
 import itertools
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from elasticsearch import Elasticsearch
+import requests
+ES_HOST = 'elasticsearch'
+ES_PORT = 9200
 
 es = Elasticsearch(
-    ['localhost'],
-    port=9200,
+    [ES_HOST],
+    port=ES_PORT,
 )
-
 app = Flask(__name__)
 
 
 @app.route('/search')
 def search(methods=['GET']):
-    search_term = request.args.get('search', '')
-    if 'vs' in search_term:
-        search_terms = search_term.split("vs")
-        list1 = search_in_elasticsearch(search_terms[0])
-        list2 = search_in_elasticsearch(search_terms[1])
-        result = list(itertools.product(list1,list2))
-    else:
-        result = search_in_elasticsearch(search_term)
-    return result
+#    search_term = request.args.get('search', '')
+#    if 'vs' in search_term:
+#        search_terms = search_term.split("vs")
+#        list1 = search_in_elasticsearch(search_terms[0])
+#        list2 = search_in_elasticsearch(search_terms[1])
+#        result = list(itertools.product(list1,list2))
+#    else:
+#        result = search_in_elasticsearch(search_term)
+#    return str(result)
+    item_id = request.args.get('id', '')
+    item_id = 1
+    result = es.get(index="poc", doc_type='model', id=item_id)['_source']
+    return str(result)
 
 @app.route('/item')
 def item():
@@ -58,11 +65,19 @@ def search_in_elasticsearch(search_term):
                             }
                         }
                     }
-		]
+                ]
             }
         }
     } 
-    result = es.search(index="poc", body=query)
+
+    url = "http://{}:{}/poc/model/_search".format(ES_HOST, ES_PORT)
+    payload = json.dumps(query) 
+    headers = {
+        'content-type': "application/json",
+    }
+    response = requests.request("GET", url, data=payload, headers=headers )
+    result =  json.loads(response.text)
+    print result
     sources = []
     for doc in result["hits"]["hits"]:
         source = doc['_source']
